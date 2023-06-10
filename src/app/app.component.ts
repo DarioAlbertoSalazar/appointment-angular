6c0d867e4ea36308dddf386a66206aeed34c06af
Darío Salazar Leal <dario12_58@hotmail.com>
2023-06-09 18:08:13 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'citas';
+  listCitias: any[] = [];
+
+
+  agregarCita(cita: any) {
+    this.listCitias.push(cita);
+  }
+
+  eliminarCitaListado(index: number) {
+    this.listCitias.splice(index, 1);
+  }
 }
